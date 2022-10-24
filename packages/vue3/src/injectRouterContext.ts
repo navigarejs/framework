c3f2fd19f67d5f5b26ef1d0a636b99d627b5c36d
Julian Hundeloh <jaulz@users.noreply.github.com>
2022-10-24 16:41:02 +0200

    fix: catch undefined router

--- packages/vue3/src/injectRouterContext.ts
+++ packages/vue3/src/injectRouterContext.ts
@@ -1,9 +1,16 @@
+import { throwError } from '@navigare/core'
 import { inject } from 'vue'
 import { RouterContext } from './provideRouterContext'
 import { ContextOf } from './types'
 
 export function injectRouterContext(): ContextOf<typeof RouterContext> {
-  const context = inject(RouterContext)!
+  const context = inject(RouterContext)
+
+  if (!context) {
+    throwError(
+      'Router context is not available. Did you use "createApp" before?',
+    )
+  }
 
   return context
 }
