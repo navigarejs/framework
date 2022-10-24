1a11f7b9b0f673955a71ff398bcb6bd452553a3d
Julian Hundeloh <jaulz@users.noreply.github.com>
2022-10-24 11:51:09 +0200

    fix: test doc change

---
+++ packages/docs/src/.vitepress/dist/assets/index.md.65a90618.js
@@ -0,0 +1,11 @@
+import { _ as e, c as t, o as a, a as o } from './app.63ff3b23.js'
+const u =
+    '{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index.md"}',
+  s = {},
+  n = o('p', null, 'Test2', -1),
+  r = [n]
+function c(_, d, i, p, l, f) {
+  return a(), t('div', null, r)
+}
+var h = e(s, [['render', c]])
+export { u as __pageData, h as default }
