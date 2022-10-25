e92f5394eea514a0eaab429d56cf29fcf860ddf2
Julian Hundeloh <jaulz@users.noreply.github.com>
2022-10-25 20:56:29 +0200

    fix: fix command

--- packages/laravel/release.config.cjs
+++ packages/laravel/release.config.cjs
@@ -5,13 +5,13 @@ const configuration = require('../../release.config.cjs')('yarn prepack', [
   [
     '@semantic-release/exec',
     {
-      prepareCmd: `curl \ 
-        -X POST \
-        -H "Accept: application/vnd.github+json" \
-        -H "Authorization: Bearer ${process.env.GIT_TOKEN_LARAVEL}" \
-        https://api.github.com/repos/navigarejs/laravel/dispatches \
+      prepareCmd: `curl
+        -X POST
+        -H "Accept: application/vnd.github+json"
+        -H "Authorization: Bearer ${process.env.GIT_TOKEN_LARAVEL}"
+        https://api.github.com/repos/navigarejs/laravel/dispatches
         -d '{ "event_type": "synchronize", "client_payload": { "message": "chore(release): \${nextRelease.version} [skip ci]" } }'
-      `,
+      `.split("\n").join(' '),
     },
   ],
 ])
