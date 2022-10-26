ea6e480f295adb7db3c20084872aee3b26cee0c7
Julian Hundeloh <jaulz@users.noreply.github.com>
2022-10-26 08:38:13 +0200

    fix: pass version only

--- packages/laravel/release.config.cjs
+++ packages/laravel/release.config.cjs
@@ -23,7 +23,7 @@ const configuration = require('../../release.config.cjs')('yarn prepack', [
         -X POST
         -H "Accept: application/vnd.github+json"
         -H "Authorization: Bearer ${process.env.LARAVEL_GIT_TOKEN}"
-        -d '{ "event_type": "synchronize", "client_payload": { "message": "chore(release): \${nextRelease.version} [skip ci]" } }'
+        -d '{ "event_type": "synchronize", "client_payload": { "version": "\${nextRelease.version}" } }'
         --fail-with-body
         https://api.github.com/repos/navigarejs/laravel/dispatches
       `),
