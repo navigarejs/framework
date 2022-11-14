867e50bf23410a388743b979d74b92179bf66978
Julian Hundeloh <jaulz@users.noreply.github.com>
2022-11-14 09:16:11 +0100

    fix: reorder exports

--- packages/vue3/src/index.ts
+++ packages/vue3/src/index.ts
@@ -10,10 +10,6 @@ export {
   default as devToolsPlugin,
 } from './devToolsPlugin'
 
-export { default as mountNavigareApp, default as mountApp } from './mountApp'
-
-export { default as renderNavigareApp, default as renderApp } from './renderApp'
-
 export { Head as NavigareHead, Head } from '@vueuse/head'
 
 export { default as NavigareDeferred, default as Deferred } from './Deferred'
@@ -23,6 +19,10 @@ export { default as NavigareForm, default as Form } from './Form'
 export { default as NavigareInput, default as Input } from './Input'
 
 export { default as NavigareLink, default as Link } from './Link'
+
+export { default as mountNavigareApp, default as mountApp } from './mountApp'
+
+export { default as renderNavigareApp, default as renderApp } from './renderApp'
 
 export { default as NavigareRoot, default as Root } from './Root'
 
