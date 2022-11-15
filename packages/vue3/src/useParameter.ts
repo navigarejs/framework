9c0501af3690fe447ff90005d000ee1df5426081
Julian Hundeloh <jaulz@users.noreply.github.com>
2022-11-15 16:50:05 +0100

    fix: allow computed key

--- packages/vue3/src/useParameter.ts
+++ packages/vue3/src/useParameter.ts
@@ -1,20 +1,28 @@
 import useRouter from './useRouter'
 import { RouteParameter } from '@navigare/core'
 import get from 'lodash.get'
+import isString from 'lodash.isstring'
 import { computed } from 'vue'
 
 export default function useParameter<TParameter extends RouteParameter>(
-  key: string,
+  getKey: string | (() => string),
   defaultValue?: TParameter,
 ) {
   const router = useRouter()
+  const key = computed(() => {
+    if (isString(getKey)) {
+      return getKey
+    }
+
+    return getKey()
+  })
   const value = computed<TParameter>({
     get: () => {
-      return (get(router.parameters, key) || defaultValue) as TParameter
+      return (get(router.parameters, key.value) || defaultValue) as TParameter
     },
 
     set: (value: TParameter) => {
-      router.parameters[key] = value
+      router.parameters[key.value] = value
     },
   })
 
