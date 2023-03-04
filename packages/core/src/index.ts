6299e00c71cf81fd2787d97b7ab77f3396f64868
Julian Hundeloh <jaulz@users.noreply.github.com>
2023-03-04 17:02:06 +0100

    fix: remove modal import

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,5 @@
 export { default as route, default as createRoute } from './createRoute'
 
-export { default as Modal } from './modal'
-
 export { default as Router } from './Router'
 
 export { default as Route } from './Route'
